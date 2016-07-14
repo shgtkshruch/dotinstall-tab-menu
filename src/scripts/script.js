78ef9ea69f387a35e63810b34fcd33961dc408ce
shgtkshruch <shgtk.shruch@gmail.com>
2016-07-14 14:46:09 +0900

    Complete

--- src/scripts/script.js
+++ src/scripts/script.js
@@ -1,1 +1,26 @@
+(() => {
+  'use strict';
 
+  const menuItems = document.getElementsByClassName('tab-menu__link');
+  const contents = document.getElementsByClassName('tab-content');
+
+  for (var i = 0; i < menuItems.length; i++) {
+    menuItems[i].addEventListener('click', function (e) {
+      e.preventDefault();
+
+      for (var i = 0; i < menuItems.length; i++) {
+        menuItems[i].className = 'tab-menu__link'
+      }
+
+      this.className = 'tab-menu__link active';
+
+      for (var i = 0; i < contents.length; i++) {
+        contents[i].className = 'tab-content'
+      }
+
+      document.getElementById(this.dataset.id).className = 'tab-content active';
+
+    });
+  }
+
+})();
